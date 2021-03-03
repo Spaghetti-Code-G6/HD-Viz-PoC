6d9259d37814af1d7d52df64e7efa6ed22d447db
Archetipo95 <martin.masevski@gmail.com>
2021-03-03 11:44:56 +0100

    Improved menus

--- public_html/jquery.tabshort.js
+++ public_html/jquery.tabshort.js
@@ -1,4 +1,4 @@
-function openPage(evt, cityName) {
+function openPage(evt, clickedName) {
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
@@ -8,6 +8,6 @@ function openPage(evt, cityName) {
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
-  document.getElementById(cityName).style.display = "block";
+  document.getElementById(clickedName).style.display = "block";
   evt.currentTarget.className += " active";
 }
