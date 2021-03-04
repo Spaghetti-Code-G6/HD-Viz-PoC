fae2472f72fda5a9c90089d39dbf770d9d17a2d5
Ubriacopo <65170353+Ubriacopo@users.noreply.github.com>
2021-03-04 11:38:03 +0100

    Caricamento sessione precedente in caso di ricarica pagina/ rivisita

--- public_html/data_display.js
+++ public_html/data_display.js
@@ -1,3 +1,4 @@
+
 let dataset = [];
 
 const w = 1080;
@@ -7,10 +8,23 @@ const space_between_charts = 0;
 const vertical_space = 0;
 
 function main() {
-	const svg = d3.select("#scatter_plot_content")
-		.append("svg")
-		.attr("width", w)
-		.attr("height", h);
+ 	const svg = d3.select("#scatter_plot_content").append("svg").attr("width", w).attr("height", h);
+	lastSession();
+}
+
+
+/** Prende i dati della scorsa operazione fatta su HdViz (molto prototipo al momento in quanto sarà più grosso)
+ * 	però funziona.*/
+function lastSession(){
+
+	fetch('prevSession', {method : 'GET'})
+		.then(res => res.json())
+		.then(res =>{
+			dataset = [];
+			if(res.hdConfig === 'csv')
+				d3.csv(res.hdFilePath, data => dataset.push(data))
+					.then(() => drawScatterPlot(dataset))});
+
 }
 
 function sendData() {
@@ -27,6 +41,7 @@ function sendData() {
 		.then(response => response.json())
 		.then(json => {
 			dataset = [];
+			console.log(json.url)
 			d3.csv(json.url, (data) => {
 				dataset.push(data);
 			})
